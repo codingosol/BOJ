bde616e4592f1a1045a5490a507735026fd1c1f2
codingosol <codingosol@gmail.com>
2023-02-05 14:41:38 +0000

    Day2-3

--- Day2/10951.cpp
+++ Day2/10951.cpp
@@ -1,4 +1,12 @@
 #include <iostream>
 using namespace std;
 
-int main(void) {}
+int main(void) {
+  // 입력이 존재하지 않을 때까지 하면 됨
+  int A, B;
+  while (cin >> A) {
+    cin >> B;
+    cout << A + B << "\n";
+  }
+  return 0;
+}
