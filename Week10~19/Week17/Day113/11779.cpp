c6c794ac15ba3d600f567262329bfc921acdd902
코딩용오소리 <codingosol@gmail.com>
2023-07-03 14:52:15 +0000

    Day113

--- Week10~19/Week17/Day113/11779.cpp
+++ Week10~19/Week17/Day113/11779.cpp
@@ -10,9 +10,8 @@ int n, m; // n은 도시의 수, m은 버스의 수
 vector<pair<int,int>> v[1001]; // first는 도착 도시 번호, second는 비용
 int_64 city[1001]; // 각 도시까지의 현재 최단거리
 int visit[1001]; // 각 도시 방문여부
-vector<int> path; // 거치는 도시의 경로
+int path[1001]; // 거치는 도시의 경로
 int cnt = 0; // 거치는 총 도시 수
-int total = 0; // 거치는 총 비용
 int_64 INF = 987654321;
 
 
@@ -28,6 +27,7 @@ void input()
 
 void dijkstra(int start, int end) // start에서 end로 가는 최단 경로를 찾아줄 다익스트라
 {
+    fill_n(path,1001,0);
     fill_n(city,1001,INF); // 초기값 무한대로 설정
     fill_n(visit,1001,0);
     city[start] = 0;
@@ -49,17 +49,29 @@ void dijkstra(int start, int end) // start에서 end로 가는 최단 경로를 
             int nextp = v[curp][i].first;
             if(city[nextp] > city[curp] + nextd)
             {
+                path[nextp] = curp;
                 city[nextp] = city[curp] + nextd;
                 pq.push({city[nextp],nextp});
             }
         }
-        cout << curd << ' ' << curp << '\n';
-        for(int i = 1; i <= n; i++)
-        {
-            cout << city[i] << ' ';
-        }
-        cout << '\n';
     }
+    // 1. 경로 총 비용 출력
+    cout << city[end] << '\n';
+    // 2. 경로에 포함된 도시 개수 출력
+    int temp = end;
+    vector<int> route;
+    while(temp > 0)
+    {
+        route.push_back(temp);
+        temp = path[temp];
+    }
+    cout << route.size() << '\n';
+    // 3. 경로 순차적으로 출력
+    for(int i = route.size() - 1; i >= 0; i--)
+    {
+        cout << route[i] << ' ';
+    }
+    cout << '\n';
 }
 
 int main(void)
@@ -69,16 +81,7 @@ int main(void)
     int start, end;
     cin >> start >> end;
     dijkstra(start,end);
-    // 1. 경로 총 비용 출력
-    cout << city[end] << '\n';
-    // 2. 경로에 포함된 도시 개수 출력
-    cout << path.size() << '\n';
-    // 3. 경로 순차적으로 출력
-    for(int i = 0; i < path.size(); i++)
-    {
-        cout << path[i] << ' ';
-    }
-    cout << '\n';
     // 2는 그냥 city[end] 출력하면 그만이지만 경로는 어떻게 출력하지?
+    // 1 4 5 혹은  1 3 5가 나와야함
     return 0;
 }
